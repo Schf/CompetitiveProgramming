99bad7281bb4f3d26879647ea404a5f0db6a12b5
Franco Schmidt Rossi <francosr98@gmail.com>
2019-11-24 10:24:56 -0300

    Uncommited changes

--- AtCoder/BegginerContest145/D.cpp
+++ AtCoder/BegginerContest145/D.cpp
@@ -18,26 +18,51 @@ typedef pair<int, int> pii;
 typedef vector<int> vi;
 #define pb(x) push_back ((x))
 
-#define DEBUG 1
+#define DEBUG 0
 #define ddcout if (DEBUG) cout
 #define ddprintf(fmt, ...) if (DEBUG) printf (fmt, ##__VA_ARGS__)
+
+#define ddprintfv(v) if (DEBUG) { trav (a, v) cout << a << " "; cout << "\n"; }
+
+#define EXIT ddprintf ("Time elapsed %lf\n", 1.0 * clock() / CLOCKS_PER_SEC); exit (0)
 
 const ll   MOD   =  1e+9+7;
 const ll   INF   =  0x3f3f3f3f3f3f3f3f;
 const int  INFi  =  0x3f3f3f3f;
 const ll   MAXN  =  1e+6+7;
 
-int trig (int lvl, int pos, vi & old)
-{
-    if (!lvl)
-        return old[old.size ()/2 - pos] % MOD;
-    vi cur (old.size () + 1, 1);
-    for (int i = 1; i < old.size (); i++)
-    {
-        cur[i] = (old[i] + old[i - 1]) % MOD;
-        cur[i] %= MOD;
+template <typename T>
+T extGcd(T a, T b, T& x, T& y) {
+    if (b == 0) {
+        x = 1; y = 0; return a;
     }
-    return trig (lvl-1, pos, cur);
+    else {
+        T g = extGcd(b, a % b, y, x);
+        y -= a / b * x;
+        return g;
+    }
+}
+ 
+template <typename T>
+T modInv(T a, T m) {
+    T x, y;
+    extGcd(a, m, x, y);
+    return (x % m + m) % m;
+}
+ 
+template <typename T>
+T modDiv(T a, T b, T m) {
+    return ((a % m) * modInv(b, m)) % m;
+}
+ 
+template<typename T>
+T modMul(T a, T b, T m) {
+    T x = 0, y = a % m;
+    while (b > 0) {
+        if (b % 2 == 1) x = (x + y) % m;
+        y = (y * 2) % m; b /= 2;
+    }
+    return x % m;
 }
 
 int main ()
@@ -45,38 +70,24 @@ int main ()
     int i, j;
     int X, Y;
     scanf ("%d %d", &X, &Y);
-    if ((X + Y)%3 == 0)
+    
+    if ((X + Y)%3 != 0 || min (X, Y) * 2 < max (X, Y))
     {
-        if (min (X, Y) * 2 >= max (X, Y))
-        {
-            ll den = 1, div = 1;
-            int n = (X+Y)/3, k = (n + 1)/2 - abs (X-Y)/2;
-            ddprintf ("%d %d\n", n, k);
-            vector <ll> N (n - max (k, n - k)), K (min (k, n - k));
-            for (i = 0; i != N.size (); i++)
-                N[i] = n - i;
-            for (i = 0; i != K.size (); i++)
-                K[i] = K.size () - i;
-            trav (a, N)
-                ddprintf ("%lld ", a);
-            ddprintf ("\n");
-            trav (a, K)
-                ddprintf ("%lld ", a);
-            ddprintf ("\n");
-            trav (a, N)
-                den = (den * a) % MOD;
-            den %= MOD;
-            ddprintf ("%lld\n", den);
-            trav (a, K)
-                div = (div * a) % MOD;
-            div %= MOD;
-            ddprintf ("%lld\n", div);
-            printf ("%lld\n", (den/div) % MOD);
-            goto end;
-        }
+        printf ("0\n");
+        EXIT;
     }
-    printf ("0\n");
-    end:
-    ddprintf ("Time elapsed %lf\n", 1.0 * clock() / CLOCKS_PER_SEC);
-    return 0;
+
+    ll n = (X+Y)/3;
+    ll k = n/2 - abs (X-Y)/2;
+    ddprintf ("%lld %lld\n", n, k);
+    ll num = 1, den = 1;
+    for (i = n; i > max (k, n - k); i--)
+        num = (num * i) % MOD;
+    for (i = min (k, n - k); i > 0; i--)
+        den = (den * i) % MOD;
+    ddprintf ("%lld %lld\n", num, den);
+
+    printf ("%lld\n", modDiv (num, den, MOD));
+
+    EXIT;
 }
--- AtCoder/BegginerContest145/E.cpp
+++ AtCoder/BegginerContest145/E.cpp
@@ -1,5 +1,9 @@
 #include <bits/stdc++.h>
 
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-result"
+#pragma GCC diagnostic ignored "-Wunused-label"
+
 using namespace std;
 
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
@@ -11,21 +15,78 @@ using namespace std;
 typedef long long ll;
 
 typedef pair<int, int> pii;
-#define fi first
-#define se second
-#define mkp(x) make_pair ((x))
+#define f first
+#define s second
+#define mp(x) make_pair ((x))
 
 typedef vector<int> vi;
 #define pb(x) push_back ((x))
 
-#define DEBUG 1
+#define DEBUG 0
 #define ddcout if (DEBUG) cout
 #define ddprintf(fmt, ...) if (DEBUG) printf (fmt, ##__VA_ARGS__)
+
+#define EXIT ddprintf ("Time elapsed %lf\n", 1.0 * clock() / CLOCKS_PER_SEC); exit (0)
+
+const ll   MOD   =  1e+9+7;
+const ll   INF   =  0x3f3f3f3f3f3f3f3f;
+const int  INFi  =  0x3f3f3f3f;
+const ll   MAXN  =  1e+6+7;
+
+vector <pii> dl;
+vector <vi> dp;
+int m;
+
+int ks (int t, int w)
+{
+    ddprintf ("t:%9d | w:%9d\n", t, w);
+    if (t == sz (dl))
+        return 0;
+    if (dp[t][w+m] != -1)
+        return dp[t][w+m];
+    if (w <= 0)
+    {
+        dp[t][w+m] = ks(t + 1, w);
+        ddprintf ("r:%9d |\n", dp[t][w+m]);
+        return dp[t][w+m];
+    }
+    int v1 = ks(t + 1, w), v2 = ks(t + 1, w - dl[t].f) + dl[t].s;
+    dp[t][w+m] = max (v1, v2);
+    ddprintf ("r:%9d | 1:%9d | 2:%9d\n", dp[t][w+m], v1, v2);
+    trav (a, dp)
+    {
+        trav (b, a)
+            ddprintf ("%4d ", b);
+        ddprintf ("\n");
+    }
+    return max (v1, v2);
+}
 
 int main ()
 {
     int i, j;
-    scanf ("%d", &i);
-    printf ("%d\n", i*i);
-    return 0;
+    int N, T;
+    m = 0;
+    scanf ("%d %d", &N, &T);
+    dl.resize (N);
+    trav (a, dl)
+    {
+        scanf ("%d %d", &a.f, &a.s);
+        m = max (m, a.f);
+    }
+    sort (all (dl));
+    dp = vector <vi> (N + 1, vi (T + 1 + m, -1));
+    trav (a, dl)
+        ddprintf ("%d %d\n", a.f, a.s);
+    trav (a, dp)
+    {
+        trav (b, a)
+            ddprintf ("%d ", b);
+        ddprintf ("\n");
+    }
+    ddprintf ("\n");
+    printf ("%d\n", ks (0, T));
+    
+
+    EXIT;
 }
