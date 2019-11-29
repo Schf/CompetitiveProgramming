c8cfd627864c61e23e678773dbb2db660a00dbca
Franco Schmidt Rossi <francosr98@gmail.com>
2019-11-29 06:48:30 -0300

    Adding pragmas, changing position of some defines for better reading

--- template.cpp
+++ template.cpp
@@ -1,38 +1,45 @@
+#pragma GCC optimization ("O2") // "Ofast" for REAL FAST
+// #pragma GCC optimization ("unroll-loops")
+
+// #pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-result"
+#pragma GCC diagnostic ignored "-Wunused-label"
+
 #include <bits/stdc++.h>
+#define DEBUG 1
 
 using namespace std;
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define trav(a, x) for(auto& (a) : (x))
+#define rep(i, a, b) for (i = (a); i < (b); i++)
+#define trav(a, x) for (auto & a : x)
 #define all(x) (x).begin (), (x).end ()
 #define sz(x) (int) (x).size ()
 #define FASTIO cin.sync_with_stdio (0); cin.tie (0); cin.exceptions (cin.failbit);
 
 typedef long long ll;
 
-typedef pair<int, int> pii;
-#define fi first
-#define se second
-#define mkp(x) make_pair ((x))
+typedef pair <int, int> pii;
+#define f first
+#define s second
+#define mp(x, y) make_pair (x, y)
 
-typedef vector<int> vi;
-#define pb(x) push_back ((x))
+typedef vector <int> vi;
+#define pb(x) push_back (x)
 
-#define DEBUG 1
 #define ddcout if (DEBUG) cout
 #define ddprintf(fmt, ...) if (DEBUG) printf (fmt, ##__VA_ARGS__)
 
 const ll   MOD   =  1e+9+7;
 const ll   INF   =  0x3f3f3f3f3f3f3f3f;
 const int  INFi  =  0x3f3f3f3f;
-const ll   MAXN  =  1e+6+7;
+const ll   MAXN  =  1e+6+7; // UPDATE
 
 int main ()
 {
+    FASTIO;
     int i, j;
-    
 
-    end:
-    ddprintf ("Time elapsed %lf\n", 1.0 * clock() / CLOCKS_PER_SEC);
     return 0;
 }
