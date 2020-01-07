bf095fbbcde8de679368f12d4aa116430c6be9e9
Franco Schmidt Rossi <francosr98@gmail.com>
2020-01-07 11:42:11 -0300

    263A - Beautiful Matrix / AC / 60ms

---
+++ Codeforces/LooseProblems/0263A.cpp
@@ -0,0 +1,51 @@
+#pragma GCC optimize ("O2")
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-result"
+#pragma GCC diagnostic ignored "-Wunused-label"
+
+#define DEBUG 0
+#define dd if (DEBUG)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define trav(a, A) for (auto & a : A)
+#define all(a) (a).begin (), (a).end ()
+#define rall(a) (a).rbegin (), (a).rend ()
+#define sz(a) (int) (a).size ()
+#define szl(a) (int) (a).length ()
+#define FASTIO cin.sync_with_stdio (0); cin.tie (0); cin.exceptions (cin.failbit);
+
+typedef long long ll;
+typedef pair <int, int> pii;
+#define f first
+#define s second
+#define mp(a, b) make_pair((a), (b))
+typedef vector <int> vi;
+#define pb(a) push_back ((a))
+
+const ll MOD = 1e+9+7;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const int INFi = 0x3f3f3f3f;
+const ll MAXN = 1e+6+7;
+
+int main ()
+{
+    FASTIO;
+    int i, j, n;
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            cin >> n;
+            if (n)
+            {
+                cout << abs (2 - j) + abs (2 - i) << "\n";
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
