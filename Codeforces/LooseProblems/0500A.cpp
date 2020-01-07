a0c24b6c27c9002bcae94b71f81f8be81f59b199
Franco Schmidt Rossi <francosr98@gmail.com>
2020-01-07 13:39:11 -0300

    577A - Multiplication Table / AC / 46ms

---
+++ Codeforces/LooseProblems/0500A.cpp
@@ -0,0 +1,62 @@
+#pragma GCC optimize ("O2")
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-result"
+#pragma GCC diagnostic ignored "-Wunused-label"
+
+#define DEBUG 0
+#define dd if (DEBUG)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define trav(a, A) for (auto & a : A)
+#define all(a) (a).begin (), (a).end ()
+#define rall(a) (a).rbegin (), (a).rend ()
+#define sz(a) (int) (a).size ()
+#define szl(a) (int) (a).length ()
+#define FASTIO cin.sync_with_stdio (0); cin.tie (0); cin.exceptions (cin.failbit);
+
+typedef long long ll;
+typedef pair <int, int> pii;
+#define f first
+#define s second
+#define mp(a, b) make_pair((a), (b))
+typedef vector <int> vi;
+#define pb(a) push_back ((a))
+
+const ll MOD = 1e+9+7;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const int INFi = 0x3f3f3f3f;
+const ll MAXN = 1e+6+7;
+
+int main ()
+{
+    FASTIO;
+    int i, j, n, t;
+    cin >> n >> t;
+    vi g (n), v (n);
+    for (i = 0; i < n; i++)
+    {
+        cin >> g[i];
+        g[i] += i;
+    }
+    for (i = 0; true;)
+    {
+        if (v[i])
+        {
+            cout << "NO";
+            break;
+        }
+        if (i == t)
+        {
+            cout << "YES";
+            break;
+        }
+        v[i] = 1;
+        i = g[i];
+    }
+    cout << "\n";
+
+    return 0;
+}
