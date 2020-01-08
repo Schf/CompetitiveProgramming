e60072aec2cd59b3b320c2f6507b65df9752df2d
Franco Schmidt Rossi <francosr98@gmail.com>
2020-01-08 05:43:58 -0300

    A - Frog 1 / AC / 9ms

---
+++ AtCoder/EducationalDPContest/A.cpp
@@ -0,0 +1,52 @@
+#pragma GCC optimize ("O2")
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-result"
+#pragma GCC diagnostic ignored "-Wunused-label"
+
+#define DEBUG 1
+#define dd if (DEBUG)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define trav(a, A) for (auto & a : A)
+#define all(a) (a).begin (), (a).end ()
+#define rall(a) (a).rbegin (), (a).rend ()
+#define sz(a) (int) (a).size ()
+#define szl(a) (int) (a).length ()
+#define FASTIO cin.sync_with_stdio (0); cin.tie (0); cin.exceptions (cin.failbit);
+
+typedef long long ll;
+typedef pair <int, int> pii;
+#define f first
+#define s second
+#define mp(a, b) make_pair((a), (b))
+typedef vector <int> vi;
+#define pb(a) push_back ((a))
+
+const ll MOD = 1e+9+7;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const int INFi = 0x3f3f3f3f;
+const ll MAXN = 1e+6+7;
+
+int main ()
+{
+    FASTIO;
+    int i, j, n;
+    cin >> n;
+    vi h (n), dp (n);
+    trav (a, h)
+        cin >> a;
+    for (i = 1; i < n; i++)
+    {
+        for (j = 1, dp[i] = INFi; j <= 2 && i - j >= 0; j++)
+        {
+            dp[i] = min (dp[i], dp[i - j] + abs (h[i] - h[i - j]));
+        }
+    }
+    cout << dp[n - 1] << "\n";
+
+    return 0;
+}
+
