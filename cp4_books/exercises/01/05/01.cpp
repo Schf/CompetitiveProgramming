b10dc1ae35effd89266133b0064b4386a897a78b
Franco Schmidt Rossi <francosr98@gmail.com>
2023-10-24 08:31:01 -0300

    CP4 book 1 Exercise 1.5.1.

---
+++ cp4_books/exercises/01/05/01.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+  char str[1024];
+  int pos = 0;
+
+  while (fgets(str + pos, 128, stdin)) {
+    if (memcmp(str + pos, ".......", 7) != 0)
+      pos += strlen(str + pos), str[pos - 1] = ' ';
+  }
+  str[pos-1] = '\0';
+
+  puts(str);
+
+  return 0;
+}
