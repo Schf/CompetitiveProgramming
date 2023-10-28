bc71b5623932674c199604d372c18eb2ca7de9ba
Franco Schmidt Rossi <francosr98@gmail.com>
2023-10-28 12:28:00 -0300

    Applying clang default formatting to template.cpp.

--- template.cpp
+++ template.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define trav(a, A) for (auto & a : A)
+#define trav(a, A) for (auto &a : A)
 #define f first
 #define s second
 
@@ -9,16 +9,15 @@ typedef long long ll;
 typedef vector<ll> vll;
 typedef pair<ll, ll> pll;
 
-const ll MOD = 1e+9+7;
+const ll MOD = 1e+9 + 7;
 const ll INFLL = 0x3f3f3f3f3f3f3f3f;
 const int INF = 0x3f3f3f3f;
-const ll MAXN = 1e+5+7;
+const ll MAXN = 1e+5 + 7;
 
-int main ()
-{
-	ios_base::sync_with_stdio (0);
-	cin.tie (0);
-	ll i, j, n;
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  ll i, j, n;
 
-	return 0;
+  return 0;
 }
